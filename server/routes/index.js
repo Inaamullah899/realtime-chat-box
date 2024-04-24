1c40f995c457cd4832be94b7909fbb88a7fd35a3
Inaamullah Bhatti <00000566a@gmail.com>
2024-04-25 01:22:51 +0500

    config of sockrt.io with server

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const socketio = require("socket.io");
+const http = require("http");
+const PORT = process.env.PORT || 6000;
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+server.listen(PORT, () => console.log(`server has started on ${PORT}`));
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
